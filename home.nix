991862028a87ce63d5da94ee89fa1516c58b5d27
Alec <alecvbnm@outlook.com>
2025-11-27 14:20:54 +0800

    fix: configuration

--- home.nix
+++ home.nix
@@ -1,11 +1,22 @@
 # Home-Manager in standalone mode
 # This is a home-manager module
 
-{ ... }: {
+{ config, ... }:
+let
+  inputs = {
+    nixpkgs-stable =
+      import <nixpkgs-stable> { config = config.nixpkgs.config; };
+  };
+in {
   imports = [
     ./shared.nix
     ./pkgs.nix
 
     ./users/alec.nix # Personal Configuration
   ];
+
+  _module.args = { inherit inputs; };
+
+  nixpkgs.config.allowUnfree = true;
+  programs.home-manager.enable = true;
 }
