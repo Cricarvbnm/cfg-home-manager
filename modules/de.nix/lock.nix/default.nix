01c15740c51a680b057c54ba23b59539423e552e
Alec <alecvbnm@outlook.com>
2025-12-01 10:54:12 +0800

    feat(lock): Use system-wide hyprlock when in generic linux

--- modules/de.nix/lock.nix/default.nix
+++ modules/de.nix/lock.nix/default.nix
@@ -1,11 +1,15 @@
 { config, ... }: {
   programs.hyprlock = {
-    enable = true;
+    enable = !config.targets.genericLinux.enable; # For broken pam
     extraConfig = ''
       source = ${config.xdg.configHome}/hypr/hyprlock/current.conf
     '';
   };
-  xdg.configFile."hypr/hyprlock/current.conf".source =
-    config.lib.file.mkOutOfStoreSymlink ./hyprlock.conf;
-  mine.de.idle.lock = "${config.programs.hyprlock.package}/bin/hyprlock";
+  xdg.configFile."hypr/${
+    if config.targets.genericLinux.enable then
+      "hyprlock.conf"
+    else
+      "hyprlock/current.conf"
+  }".source = config.lib.file.mkOutOfStoreSymlink ./hyprlock.conf;
+  mine.de.idle.lock = "hyprlock";
 }
