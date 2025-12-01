216f2ee1f14b4bda24e222eea68a016ea8da4360
Alec <alecvbnm@outlook.com>
2025-12-01 20:04:40 +0800

    feat(shell): Add some replacement of coreutils
    Replacement:
    1. cat <= bat --plain
    2. ls <= lsd
    3. diff <= git-diff with git-delta
    
    Add:
    1. bat: better cat
    2. rga: better grep
    3. fd: simplier and better find
    4. fzf: fuzzy search
    5. rsync: better cp and mv between filesystems. Add cpr and mvr as
       replacements of cp and mv
    6. duf: better du
    7. gdu: better du with console
    8. nix-output-monitor: some aliases for home-manager

--- modules/shell.nix/default.nix
+++ modules/shell.nix/default.nix
@@ -28,32 +28,28 @@ in {
     ffmpeg-headless
   ];
 
-  programs = {
-    zsh = {
-      enable = true;
+  programs.zsh = {
+    enable = true;
 
-      dotDir = "${config.xdg.configHome}/zsh";
-      initContent = mkAfter "bindkey -v";
+    dotDir = "${config.xdg.configHome}/zsh";
+    initContent = mkAfter "bindkey -v";
 
-      history = {
-        path = "${config.xdg.stateHome}/zsh/history";
-        expireDuplicatesFirst = true;
-        extended = true;
-        findNoDups = true;
-        share = false;
-      };
-
-      syntaxHighlighting = {
-        enable = true;
-        highlighters = [ "main" "brackets" ];
-      };
-
-      oh-my-zsh = {
-        enable = true;
-        plugins = [ "sudo" ];
-      };
+    history = {
+      path = "${config.xdg.stateHome}/zsh/history";
+      expireDuplicatesFirst = true;
+      extended = true;
+      findNoDups = true;
+      share = false;
     };
 
-    zoxide.enable = true;
+    syntaxHighlighting = {
+      enable = true;
+      highlighters = [ "main" "brackets" ];
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "sudo" ];
+    };
   };
 }
