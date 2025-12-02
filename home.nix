a821c366c8cbf9168836d6c97c3494636c94771d
Alec <alecvbnm@outlook.com>
2025-12-02 13:22:01 +0800

    feat(channels): Use nix.channels to manage channels/inputs

--- home.nix
+++ home.nix
@@ -1,8 +1,17 @@
 # Home-Manager in standalone mode
 # This is a home-manager module
 
-{ ... }:
-let inputs = { };
+{ lib, pkgs, ... }:
+let
+  channelURLs = {
+    nixpkgs = "https://channels.nixos.org/nixos-25.11/nixexprs.tar.xz";
+    home-manager =
+      "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
+  };
+  channels = lib.mapAttrs (name: url: builtins.fetchTarball url) channelURLs;
+  inputs =
+    lib.mapAttrs (name: channel: import channel { inherit (pkgs) config; })
+    channels;
 in {
   imports = [
     ./shared.nix
@@ -13,6 +22,7 @@ in {
     ./users/alec.nix # Personal Configuration
   ];
 
+  nix.channels = channels;
   _module.args = { inherit inputs; };
 
   nixpkgs.config.allowUnfree = true;
