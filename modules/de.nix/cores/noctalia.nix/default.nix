d37bdeec21685aed43ecb56eea39b036ebe1375b
Alec <alecvbnm@outlook.com>
2025-12-02 19:00:54 +0800

    feat(de/cores): Add noctalia with niri

---
+++ modules/de.nix/cores/noctalia.nix/default.nix
@@ -0,0 +1,19 @@
+{ config, ... }: {
+  imports = [
+    ./niri.nix
+
+    ../modules/idle/hypridle.nix
+    ../modules/clipboard/copyq.nix
+    ../modules/polkit/hyprpolkitagent.nix
+  ];
+
+  xdg.configFile.noctalia.source =
+    config.lib.file.mkOutOfStoreSymlink ./noctalia;
+
+  programs.cava.enable = true;
+  mine.de.idle = {
+    displays.niri = on:
+      "niri msg action power-${if on then "on" else "off"}-monitors";
+    lock = "hyprlock";
+  };
+}
