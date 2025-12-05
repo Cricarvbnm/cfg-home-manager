b3bc16d084febeed4a99bb43a8fc37b7697cd17c
Alec <alecvbnm@outlook.com>
2025-12-05 17:45:50 +0800

    fix(idle): Use hyprland which is unavailable when using noctalia

--- modules/de.nix/cores/noctalia.nix/default.nix
+++ modules/de.nix/cores/noctalia.nix/default.nix
@@ -14,6 +14,6 @@
   mine.de.idle = {
     displays.niri = on:
       "niri msg action power-${if on then "on" else "off"}-monitors";
-    lock = "hyprlock";
+    lock = "qc -c noctalia-shell ipc call lockScreen lock";
   };
 }
