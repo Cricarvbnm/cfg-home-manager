b5cc1d0c425dd1bbfbec3854fbf4f31538e5432a
Alec <alecvbnm@outlook.com>
2025-12-18 23:45:43 +0800

    feat(generic-linux/gpu): Update to 580.119.02

--- hosts/generic-linux/nvidia.nix
+++ hosts/generic-linux/nvidia.nix
@@ -3,8 +3,10 @@
   targets.genericLinux.gpu = {
     nvidia = {
       enable = true;
-      version = "580.105.08";
-      sha256 = "sha256-2cboGIZy8+t03QTPpp3VhHn6HQFiyMKMjRdiV2MpNHU=";
+      version = "580.119.02";
+
+      # nix store prefetch-file https://download.nvidia.com/XFree86/Linux-x86_64/@VERSION@/NVIDIA-Linux-x86_64-@VERSION@.run
+      sha256 = "sha256-gCD139PuiK7no4mQ0MPSr+VHUemhcLqerdfqZwE47Nc=";
     };
   };
   nixpkgs.config.nvidia.acceptLicense = true;
