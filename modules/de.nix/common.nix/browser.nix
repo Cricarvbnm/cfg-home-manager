dffcfd995efab53b8a7fe30238ca5328b96cd7f5
Alec <alechron@outlook.com>
2026-01-15 22:46:16 +0800

    feat(de/common/browser): Microsoft Edge

--- modules/de.nix/common.nix/browser.nix
+++ modules/de.nix/common.nix/browser.nix
@@ -1,7 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.firefox = {
     enable = true;
     languagePacks = [ "zh-CN" ];
   };
+
+  home.packages = with pkgs; [
+    (microsoft-edge.override {
+      commandLineArgs = [
+        "--enable-features=UseOzonePlatform"
+        "--ozone-platform=wayland"
+      ];
+    })
+  ];
 }
